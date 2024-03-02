696ec1862f49caae732aef7894c11c2cbf877c56
ochi <ocos54ce@gmail.com>
2024-03-02 09:54:12 +0900

    remove println!

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -3,8 +3,6 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("with text: \n{contents}");
-
     Ok(())
 }
 
